2fa1fc3b91589c8d5a89de8d4d1875cb6d4f4a3b
vuthpov <42206316+vuthpov@users.noreply.github.com>
2018-11-04 20:01:55 +0700

    x

--- Martsystem/MartSystem/supplyer.Designer.cs
+++ Martsystem/MartSystem/supplyer.Designer.cs
@@ -98,7 +98,7 @@
             this.panel7.Controls.Add(this.button6);
             this.panel7.Controls.Add(this.txtSearch);
             this.panel7.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel7.Location = new System.Drawing.Point(3, 23);
+            this.panel7.Location = new System.Drawing.Point(3, 28);
             this.panel7.Name = "panel7";
             this.panel7.Size = new System.Drawing.Size(414, 36);
             this.panel7.TabIndex = 1;
@@ -172,7 +172,7 @@
             this.panel4.Controls.Add(this.btnedit);
             this.panel4.Controls.Add(this.btnadd);
             this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel4.Location = new System.Drawing.Point(5, 246);
+            this.panel4.Location = new System.Drawing.Point(5, 251);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(401, 37);
             this.panel4.TabIndex = 5;
@@ -251,7 +251,7 @@
             this.panel3.Controls.Add(this.lblID);
             this.panel3.Controls.Add(this.label2);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel3.Location = new System.Drawing.Point(5, 27);
+            this.panel3.Location = new System.Drawing.Point(5, 32);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(401, 219);
             this.panel3.TabIndex = 4;
@@ -261,7 +261,7 @@
             this.label7.AutoSize = true;
             this.label7.Location = new System.Drawing.Point(130, 146);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(14, 21);
+            this.label7.Size = new System.Drawing.Size(15, 23);
             this.label7.TabIndex = 11;
             this.label7.Text = ":";
             // 
@@ -270,7 +270,7 @@
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(130, 107);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(14, 21);
+            this.label6.Size = new System.Drawing.Size(15, 23);
             this.label6.TabIndex = 12;
             this.label6.Text = ":";
             // 
@@ -279,7 +279,7 @@
             this.label16.AutoSize = true;
             this.label16.Location = new System.Drawing.Point(130, 74);
             this.label16.Name = "label16";
-            this.label16.Size = new System.Drawing.Size(14, 21);
+            this.label16.Size = new System.Drawing.Size(15, 23);
             this.label16.TabIndex = 13;
             this.label16.Text = ":";
             // 
@@ -288,7 +288,7 @@
             this.label12.AutoSize = true;
             this.label12.Location = new System.Drawing.Point(130, 39);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(14, 21);
+            this.label12.Size = new System.Drawing.Size(15, 23);
             this.label12.TabIndex = 14;
             this.label12.Text = ":";
             // 
@@ -297,7 +297,7 @@
             this.label10.AutoSize = true;
             this.label10.Location = new System.Drawing.Point(129, 11);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(14, 21);
+            this.label10.Size = new System.Drawing.Size(15, 23);
             this.label10.TabIndex = 15;
             this.label10.Text = ":";
             // 
@@ -315,7 +315,7 @@
             this.txtEmail.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtEmail.Location = new System.Drawing.Point(159, 108);
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(234, 27);
+            this.txtEmail.Size = new System.Drawing.Size(234, 32);
             this.txtEmail.TabIndex = 16;
             // 
             // label5
@@ -323,7 +323,7 @@
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(11, 143);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(72, 21);
+            this.label5.Size = new System.Drawing.Size(88, 23);
             this.label5.TabIndex = 4;
             this.label5.Text = "Address";
             // 
@@ -332,7 +332,7 @@
             this.txtPhoneNumber.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtPhoneNumber.Location = new System.Drawing.Point(159, 75);
             this.txtPhoneNumber.Name = "txtPhoneNumber";
-            this.txtPhoneNumber.Size = new System.Drawing.Size(234, 27);
+            this.txtPhoneNumber.Size = new System.Drawing.Size(234, 32);
             this.txtPhoneNumber.TabIndex = 17;
             this.txtPhoneNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPhoneNumber_KeyPress);
             // 
@@ -341,7 +341,7 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(11, 104);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(51, 21);
+            this.label3.Size = new System.Drawing.Size(62, 23);
             this.label3.TabIndex = 5;
             this.label3.Text = "Email";
             // 
@@ -350,7 +350,7 @@
             this.label8.AutoSize = true;
             this.label8.Location = new System.Drawing.Point(11, 71);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(92, 21);
+            this.label8.Size = new System.Drawing.Size(113, 23);
             this.label8.TabIndex = 6;
             this.label8.Text = "Telephone";
             // 
@@ -359,7 +359,7 @@
             this.txtSupplierName.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtSupplierName.Location = new System.Drawing.Point(159, 42);
             this.txtSupplierName.Name = "txtSupplierName";
-            this.txtSupplierName.Size = new System.Drawing.Size(234, 27);
+            this.txtSupplierName.Size = new System.Drawing.Size(234, 32);
             this.txtSupplierName.TabIndex = 7;
             // 
             // label4
@@ -367,7 +367,7 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(11, 36);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(123, 21);
+            this.label4.Size = new System.Drawing.Size(154, 23);
             this.label4.TabIndex = 8;
             this.label4.Text = "Supplier Name";
             // 
@@ -376,7 +376,7 @@
             this.lblID.AutoSize = true;
             this.lblID.Location = new System.Drawing.Point(160, 11);
             this.lblID.Name = "lblID";
-            this.lblID.Size = new System.Drawing.Size(27, 21);
+            this.lblID.Size = new System.Drawing.Size(30, 23);
             this.lblID.TabIndex = 9;
             this.lblID.Text = "ID";
             // 
@@ -385,7 +385,7 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(10, 9);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(107, 21);
+            this.label2.Size = new System.Drawing.Size(135, 23);
             this.label2.TabIndex = 10;
             this.label2.Text = "Employee ID";
             // 
@@ -425,7 +425,7 @@
             // 
             // supplyer
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(830, 541);
