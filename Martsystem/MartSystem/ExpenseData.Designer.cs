7e70e4e6371713788919582d88d6d6ff662a6b37
vuthpov <42206316+vuthpov@users.noreply.github.com>
2018-11-05 16:58:52 +0700

    k

---
+++ Martsystem/MartSystem/ExpenseData.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace MartSystem
+{
+    partial class ExpenseData
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox7 = new System.Windows.Forms.GroupBox();
+            this.rndDateCreated = new System.Windows.Forms.RadioButton();
+            this.rndID = new System.Windows.Forms.RadioButton();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.dtDate = new System.Windows.Forms.DateTimePicker();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dgvInvoiceData = new System.Windows.Forms.DataGridView();
+            this.groupBox7.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInvoiceData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox7
+            // 
+            this.groupBox7.Controls.Add(this.rndDateCreated);
+            this.groupBox7.Controls.Add(this.rndID);
+            this.groupBox7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox7.Location = new System.Drawing.Point(4, 42);
+            this.groupBox7.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox7.Name = "groupBox7";
+            this.groupBox7.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox7.Size = new System.Drawing.Size(615, 88);
+            this.groupBox7.TabIndex = 0;
+            this.groupBox7.TabStop = false;
+            this.groupBox7.Text = "លក្ខខណ្ឌ";
+            // 
+            // rndDateCreated
+            // 
+            this.rndDateCreated.AutoSize = true;
+            this.rndDateCreated.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rndDateCreated.Location = new System.Drawing.Point(87, 44);
+            this.rndDateCreated.Margin = new System.Windows.Forms.Padding(4);
+            this.rndDateCreated.Name = "rndDateCreated";
+            this.rndDateCreated.Size = new System.Drawing.Size(169, 27);
+            this.rndDateCreated.TabIndex = 0;
+            this.rndDateCreated.TabStop = true;
+            this.rndDateCreated.Text = "Date Created";
+            this.rndDateCreated.UseVisualStyleBackColor = true;
+            // 
+            // rndID
+            // 
+            this.rndID.AutoSize = true;
+            this.rndID.Checked = true;
+            this.rndID.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rndID.Location = new System.Drawing.Point(28, 44);
+            this.rndID.Margin = new System.Windows.Forms.Padding(4);
+            this.rndID.Name = "rndID";
+            this.rndID.Size = new System.Drawing.Size(51, 27);
+            this.rndID.TabIndex = 0;
+            this.rndID.TabStop = true;
+            this.rndID.Text = "ID";
+            this.rndID.UseVisualStyleBackColor = true;
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.panel7);
+            this.groupBox6.Controls.Add(this.groupBox7);
+            this.groupBox6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox6.Font = new System.Drawing.Font("Khmer SN Kampot", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox6.Location = new System.Drawing.Point(0, 0);
+            this.groupBox6.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox6.Size = new System.Drawing.Size(623, 198);
+            this.groupBox6.TabIndex = 9;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "ស្វែងរក";
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.dtDate);
+            this.panel7.Controls.Add(this.btnCancel);
+            this.panel7.Controls.Add(this.btnSearch);
+            this.panel7.Controls.Add(this.txtSearch);
+            this.panel7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel7.Location = new System.Drawing.Point(4, 130);
+            this.panel7.Margin = new System.Windows.Forms.Padding(4);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(615, 44);
+            this.panel7.TabIndex = 1;
+            // 
+            // dtDate
+            // 
+            this.dtDate.CustomFormat = "dd/MM/yyyy";
+            this.dtDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtDate.Location = new System.Drawing.Point(0, -1);
+            this.dtDate.Name = "dtDate";
+            this.dtDate.Size = new System.Drawing.Size(300, 45);
+            this.dtDate.TabIndex = 8;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnCancel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.ForeColor = System.Drawing.Color.White;
+            this.btnCancel.Location = new System.Drawing.Point(428, 0);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(128, 44);
+            this.btnCancel.TabIndex = 7;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.Fuchsia;
+            this.btnSearch.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearch.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.ForeColor = System.Drawing.Color.White;
+            this.btnSearch.Location = new System.Drawing.Point(300, 0);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(128, 44);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Dock = System.Windows.Forms.DockStyle.Left;
+            this.txtSearch.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(0, 0);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSearch.Multiline = true;
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(300, 44);
+            this.txtSearch.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dgvInvoiceData);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(623, 413);
+            this.panel1.TabIndex = 10;
+            // 
+            // dgvInvoiceData
+            // 
+            this.dgvInvoiceData.AllowUserToAddRows = false;
+            this.dgvInvoiceData.AllowUserToDeleteRows = false;
+            this.dgvInvoiceData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvInvoiceData.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvInvoiceData.Location = new System.Drawing.Point(0, 0);
+            this.dgvInvoiceData.Name = "dgvInvoiceData";
+            this.dgvInvoiceData.ReadOnly = true;
+            this.dgvInvoiceData.RowTemplate.Height = 24;
+            this.dgvInvoiceData.Size = new System.Drawing.Size(623, 413);
+            this.dgvInvoiceData.TabIndex = 0;
+            // 
+            // ExpenseData
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(623, 413);
+            this.Controls.Add(this.groupBox6);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.ForeColor = System.Drawing.Color.Navy;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "ExpenseData";
+            this.Text = "ExpenseData";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.groupBox7.ResumeLayout(false);
+            this.groupBox7.PerformLayout();
+            this.groupBox6.ResumeLayout(false);
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInvoiceData)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox7;
+        private System.Windows.Forms.RadioButton rndDateCreated;
+        private System.Windows.Forms.RadioButton rndID;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.DateTimePicker dtDate;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dgvInvoiceData;
+    }
+}
--- Martsystem/MartSystem/Program.cs
+++ Martsystem/MartSystem/Program.cs
@@ -16,11 +16,10 @@ namespace MartSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-<<<<<<< HEAD
-            Application.Run(new Product());
-=======
-            Application.Run(new CreateInvoice());
->>>>>>> 892a5705db46e17067d74c8183bf7f045875dc44
+
+
+            Application.Run(new CreateExpense());
+
         }
     }
 }
