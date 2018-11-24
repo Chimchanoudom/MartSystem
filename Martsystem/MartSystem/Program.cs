270f08f54f1d592e6b287ea9c0ae4aed6b6460f3
vuthpov <42206316+vuthpov@users.noreply.github.com>
2018-11-24 09:03:03 +0700

    delete stock

--- Martsystem/MartSystem/Program.cs
+++ Martsystem/MartSystem/Program.cs
@@ -17,7 +17,7 @@ namespace MartSystem
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            Application.Run(new CreateInvoice());
+            Application.Run(new Stock());
 
         }
     }
--- Martsystem/MartSystem/Stock.cs
+++ Martsystem/MartSystem/Stock.cs
@@ -16,14 +16,16 @@ namespace MartSystem
         public Stock()
         {
             InitializeComponent();
+
         }
 
 
         DataTable dtStock = new DataTable();
-
+        string sql;
         private void Stock_Load(object sender, EventArgs e)
         {
-            string sql = "select stockid 'Stock ID', ProName 'Product Name',importDate 'Import Date',s.Qty 'Quantity',s.UnitPrice 'Unit Price',ExpiredDate 'Expire Date' from stock s join Product p on s.ProID=p.ProID join Import i on s.ImportID=i.ImportID ;";
+             sql = "select stockid 'Stock ID', ProName 'Product Name',importDate 'Import Date',s.Qty 'Quantity',s.UnitPrice 'Unit Price',ExpiredDate 'Expire Date' ,case when ExpiredDate<=(select convert(date,GETDATE())) then '1' else '0' end 'isExpired',case when (ExpiredDate<=(select convert(date,GETDATE()+30)) and ExpiredDate>(select convert(date,GETDATE())))  then '1' else '0' end 'isAlmostExpired' from stock s join Product p on s.ProID=p.ProID join Import i on s.ImportID=i.ImportID ;";
+
 
             SqlDataAdapter dataAdaptor = new SqlDataAdapter(sql,dataCon.Con);
 
@@ -31,34 +33,65 @@ namespace MartSystem
 
             dgvStock.DataSource = dtStock;
 
+           
+            dgvStock.Columns["isExpired"].Visible = 
+                dgvStock.Columns["isAlmostExpired"].Visible = false;
+
+           
+
             dgvStock.Columns["Import Date"].DefaultCellStyle.Format =
                 dgvStock.Columns["Expire Date"].DefaultCellStyle.Format
                 = "dd/MM/yyyy";
+            ChangeRowForeColor();
+
+
+
+        }
+
+        void ChangeRowForeColor()
+        {
+
+            foreach (DataGridViewRow row in dgvStock.Rows)
+            {
+                if (row.Cells["isExpired"].Value + "" == "1")
+                    row.DefaultCellStyle.ForeColor = Color.Red;
+                else if (row.Cells["isAlmostExpired"].Value + "" == "1")
+                    row.DefaultCellStyle.ForeColor = Color.Gold;
+            }
         }
 
         private void btnSearch_Click(object sender, EventArgs e)
         {
             string filter = "";
             if (rndProductName.Checked) filter = "[Product Name]=";
-            
-
-
 
             if (rndImportDate.Checked)
             {
                 string[] st = txtSearch.Text.Split('-');
                 filter = "[Import Date]>='" + st[0] + "' AND [Import Date] <= '" + st[1] + "'";
             }
-            else if (rndProductName.Checked) filter += "'" + txtSearch.Text + "'";
             else if (rndExpiredDate.Checked)
             {
                 string[] st = txtSearch.Text.Split('-');
                 filter = "[Expire Date]>='" + st[0] + "' AND [Expire Date] <= '" + st[1] + "'";
             }
+            else
+            {
+               if (rndProductName.Checked) filter += "'" + txtSearch.Text + "'";
+               else if (rndExpired.Checked) filter = "[isExpired]='1'";
+               else if (rndAlmostExpired.Checked) filter = "[isAlmostExpired]='1'";
+            }
 
-
-
-            dtStock.DefaultView.RowFilter = filter;
+            try
+            {
+                dtStock.DefaultView.RowFilter = filter;
+                ChangeRowForeColor();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Encounter Error");
+            }
+            
         }
 
         private void rndProductName_CheckedChanged(object sender, EventArgs e)
@@ -69,6 +102,7 @@ namespace MartSystem
         private void btnCancel_Click(object sender, EventArgs e)
         {
             dtStock.DefaultView.RowFilter = string.Empty;
+            ChangeRowForeColor();
         }
 
         private void rndImportDate_Click(object sender, EventArgs e)
@@ -78,6 +112,42 @@ namespace MartSystem
 
             if (searchDate.DialogResult != DialogResult.Yes)
                 rndProductName.Checked = true;
+        }
+
+        private void dgvStock_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvStock.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dgvStock.Rows[dgvStock.SelectedRows[0].Index];
+                deleteToolStripMenuItem.Visible = selectedRow.DefaultCellStyle.ForeColor == Color.Red;
+            }
+            
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Are you sure you want to delete selected row?", "", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (dialogResult == DialogResult.Yes)
+            {
+                int selectedIndex = dgvStock.SelectedRows[0].Index;
+                string stockID = dgvStock.Rows[selectedIndex].Cells["Stock ID"].Value + "";
+                sql = "delete from stock where stockID='" + stockID + "'";
+
+                bool error = false;
+                dataCon.ExecuteActionQry(sql, ref error);
+
+                if (!error)
+                {
+                    MessageBox.Show("Delete sucessful", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    dtStock.Rows.RemoveAt(selectedIndex);
+                }
+            }
+        }
+
+        private void rndExpired_Click(object sender, EventArgs e)
+        {
+            btnSearch_Click(null, null);
         }
     }
 }
